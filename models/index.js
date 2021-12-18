d23d28726c7ae745efd256864bda280eabb47ff2
mogannam <JoeMogannam@gmail.com>
2021-12-17 20:03:12 -0800

    init project

---
+++ models/index.js
@@ -0,0 +1,36 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  //as: 'product_tags',
+  //foreignKey: 'id'
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+   through: ProductTag,
+  // as: 'product_tags',
+  // foreignKey: 'id'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
